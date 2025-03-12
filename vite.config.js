64891a2ba2fa6fd4ee893196eb359805e91f77e8
Joel <joel960801@gmail.com>
2025-03-12 11:01:04 +0200

    update vite and tailwind

--- vite.config.js
+++ vite.config.js
@@ -1,29 +1,11 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import path from "path";
-import tailwindcss from "@tailwindcss/vite";
-import { fileURLToPath } from "url";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+import {defineConfig} from "vite"
+import react from "@vitejs/plugin-react"
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  build: {
-    lib: {
-      entry: path.resolve(__dirname, "src/index.js"),
-      name: "MyComponentLib",
-      fileName: (format) => `index.${format === "es" ? "js" : "cjs.js"}`,
-      formats: ["es", "cjs"],
-    },
-    rollupOptions: {
-      external: ["react", "react-dom"],
-      output: {
-        globals: {
-          react: "React",
-          "react-dom": "ReactDOM",
-        },
-      },
-    },
-  },
-});
+    plugins: [react()],
+    base: "/",
+    build: {
+        outDir: "dist",
+        assetsDir: "assets"
+    }
+})
